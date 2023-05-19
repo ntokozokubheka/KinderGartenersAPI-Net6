96cf7e5fa029b64671eb4ae6dece8d5cff65152f
Stokza <37531253+ntokozokubheka@users.noreply.github.com>
2023-05-19 15:35:09 +0200

    Add files via upload

---
+++ Models/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using KindergartenAPI.Models;
+
+namespace KindergartenAPI.Models
+{
+    public class ApplicationDBContext:DbContext
+    {
+
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<ChildModel> Children { get; set; } 
+
+    }
+}
---
+++ Models/ChildModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace KindergartenAPI.Models
+{
+
+    [Table("Children")]
+    public class ChildModel 
+    {
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int ChildId { get; set; }
+   
+             [Required]
+            [Column(TypeName = "nvarchar(250)")]
+            public string Name{ get; set; }="";
+            public int ContactNumber { get; set; }
+            public int age { get; set; }
+
+        
+
+    }
+}
